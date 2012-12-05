d91d548f1e21ff5b1410c70984f22def74ef2372
Andy <andy@unknown0019e3d5418c.gateway.2wire.net>
2012-12-04 21:21:14 -0500

    command line resets on 'quit' - end with control c

--- Eliza/src/fortress/CommandLine.java
+++ Eliza/src/fortress/CommandLine.java
@@ -4,21 +4,15 @@ import Eliza.*;
 import java.io.*;
 
 public class CommandLine {
-	public static String readLine() throws Exception{
-		String s = "";
-		try {
-			InputStreamReader converter = new InputStreamReader(System.in);
-			BufferedReader in = new BufferedReader(converter);
-			s = in.readLine();
-		} catch (Exception e) {
-			System.out.println("Error! Exception: "+e); 
-		}
-		return s;
-	}
-		
-	public static void main(String args[]) throws Exception {
+	public static void chat() throws Exception{
 		ElizaMain eliza = new ElizaMain();
 		eliza.readScript(true, "script");
 		eliza.runProgramOnStream(System.in);
+	}
+		
+	public static void main(String args[]) throws Exception {
+		while(true) {
+			chat();
+		}
 	}
 }
